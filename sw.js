8ed00fce313942ec28ba7e1b9c2512af4bcfc5c2
lessonplannerplus-star <lessonplannerplus@gmail.com>
2026-05-23 11:47:23 +0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'lesson-planner-v1';
+
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
